23a7b37eb82d26acc55bd60ddddc981d7e33ec48
Erik Gjesfjeld <erik.gjesfjeld@gmail.com>
2016-07-15 16:29:17 -0700

    Discrete trait for car model data
    Each manufacturer is given a unique number which is used as a discrete
    trait in the PyRate analysis

--- PyRate_CE_Functions.R
+++ PyRate_CE_Functions.R
@@ -53,6 +53,46 @@ summary_plots<-function(x,y){
   par(mfrow=c(1,1))
 }
 
+occur_to_lifespan<-function(x){
+  x.1<-x
+  colnames(x.1)<-c("year","make","model")
+  x.1[,2]<-str_c(tolower(str_trim(x[,2])))
+  x.1[,3]<-str_c(tolower(str_trim(x[,3])))
+  x.1[,2]<-str_replace_all(string=x[,2], pattern=" ", repl="_")
+  x.1[,3]<-str_replace_all(string=x[,3], pattern=" ", repl="_")
+  lifespan <-  x.1 %>%
+    arrange(year) %>%
+    group_by(make,model) %>%
+    summarise(first_year=first(year), last_year=last(year))
+  colnames(lifespan)<-c(names(x[,2:3]),"first_year","last_year")
+  raw_lifespan<<-lifespan
+  }
+
+pyrate_fixed<-function(x){
+  x.1<-x
+  colnames(x.1)<-c("make","model","first_year","last_year")
+  fixed_pyrate_data<<-data.frame(clade=as.numeric(factor(x.1$make)),
+                               species=as.numeric(factor(x.1$model)),
+                               ts=(max(x.1$last_year)-x.1$first_year),
+                               te=(max(x.1$last_year)-x.1$last_year))
+  fixed_make_key<-data.frame(make_name=x.1$make,make_no=as.numeric(factor(x.1$make))) %>% group_by(make_name) %>% summarize(No=first(make_no), count=n()) %>% arrange(No)
+  colnames(fixed_make_key)<-c(names(x[,1]),"No","Count")
+  fixed_model_key<-data.frame(model_name=x.1$model,model_no=as.numeric(factor(x.1$model))) %>% arrange(model_no)
+  colnames(fixed_model_key)<-c(names(x[,2]),"No")
+  write.table(fixed_make_key,"~/Desktop/PyRate_CE_Tutorial/pyrate_fixed_make_key.csv",quote=FALSE,sep=",",row.names = FALSE)
+  write.table(fixed_model_key,"~/Desktop/PyRate_CE_Tutorial/pyrate_fixed_model_key.csv",quote=FALSE,sep=",",row.names = FALSE)
+}
+
+pyrate_preservation<-function(x){
+  x$status<-"extinct"
+  x$status[which(x$last_year>=max(x$last_year))]<-"extant"
+  preservation_pyrate_data<<-data.frame(Species=x[,2],
+                                        Status=x$status,
+                                        min_age=max(x$last_year)-x$last_year,
+                                        max_age=max(x$last_year)-x$first_year)
+  write.table(preservation_pyrate_data,"~/Desktop/PyRate_CE_Tutorial/preservation_pyrate_data.txt",sep="\t",quote=FALSE,row.names=FALSE)
+  }
+
 
 
 
